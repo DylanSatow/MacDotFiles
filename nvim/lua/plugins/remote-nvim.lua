2920885d83af0406e95851523873bd5bd87cf31e
DylanSatow <dylansatow531@gmail.com>
2025-06-26 12:26:25 -0400

    Fixed remote nvim

--- nvim/lua/plugins/remote-nvim.lua
+++ nvim/lua/plugins/remote-nvim.lua
@@ -7,6 +7,24 @@ return {
       "MunifTanjim/nui.nvim", -- To build the plugin UI
       "nvim-telescope/telescope.nvim", -- For picking b/w different remote methods
     },
-    config = true,
+    config = function()
+      require("remote-nvim").setup({
+        client_callback = function(port, workspace_config)
+          local cmd = ("wezterm cli set-tab-title --pane-id $(wezterm cli spawn nvim --server localhost:%s --remote-ui) %s"):format(
+            port,
+            ("'Remote: %s'"):format(workspace_config.host)
+          )
+          if vim.env.TERM == "xterm-kitty" then
+            cmd = ("kitty -e nvim --server localhost:%s --remote-ui"):format(port)
+          end
+          vim.fn.jobstart(cmd, {
+            detach = true,
+            on_exit = function(job_id, exit_code, event_type)
+              print("Client", job_id, "exited with code", exit_code, "Event type:", event_type)
+            end,
+          })
+        end,
+      })
+    end,
   },
 }
